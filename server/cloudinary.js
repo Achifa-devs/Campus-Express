f74178963eed2fb285f254fac88455f8518a4b39
Achifa-dev <achifa.io.llc@gmail.com>
2024-07-20 12:14:28 +0100

    123456780

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const uploadVideo = async (filePaths, refs, folder) => {
   try {
     const uploadPromises = filePaths.map(async (filePath) => {
-      const result = await cloudinary.uploader.upload_large(filePath, {
+      const result = await cloudinary.uploader.upload(filePath, {
         resource_type: 'video',
         folder: folder,
         public_id: refs, // Use corresponding reference for each file
