7f140abc67db68db2110f075999e0aca572c3a09
Dream <130993303+Achifa-devs@users.noreply.github.com>
2025-05-17 10:24:20 +0100

    Update route.js

--- site/next/app/api/vendor/shop/route.js
+++ site/next/app/api/vendor/shop/route.js
@@ -8,7 +8,7 @@ export async function POST(req) {
     const { user_id } = body;
     console.log(user_id)
 
-    const res = await pool.query(`SELECT * FROM shopss WHERE user_id='${user_id}'`);
+    const res = await pool.query(`SELECT * FROM shops WHERE user_id='${user_id}'`);
     return NextResponse.json({ bool: res?.rows?.length > 0 ? true : false, shop: res?.rows[0] }, { status: 200 });
 
   } catch (err) {
