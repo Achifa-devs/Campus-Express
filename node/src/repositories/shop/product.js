cb3308315e7ad39d7afecc18cb2be91d6a9eb16f
Achifa-devs <akpulufabian@gmail.com>
2025-08-24 16:29:11 +0100

    new update

--- node/src/repositories/shop/product.js
+++ node/src/repositories/shop/product.js
@@ -1,29 +1,25 @@
-// import pool from "../config/db.js"
-
 import pool from "../../config/db.js";
-import shortId from "short-id"
+import shortId from "short-id";
 import { errorHandler } from "../../utils/erroHandler.js";
 
 // Find product by ID
 export async function findProductById({ product_id }) {
-    // console.log('product_id: ', product_id)
-    const result = await pool.query(
+  const result = await pool.query(
     `SELECT * FROM products WHERE product_id = $1`,
     [product_id]
-    );
+  );
   return result.rows;
 };
 
 // Find products
 export async function findProducts({ limit, campus }) {
-  if (limit !== null) {
+  if (campus === null) {
     const result = await pool.query(
       `SELECT * 
        FROM products 
        WHERE state->>'state' = 'active' 
-       AND (campus = $1 OR $1 IS NULL)
-       LIMIT $2`,
-      [campus, limit]
+       LIMIT $1`,
+      [limit]
     );
     return result.rows;
   } else {
@@ -31,8 +27,9 @@ export async function findProducts({ limit, campus }) {
       `SELECT * 
        FROM products 
        WHERE state->>'state' = 'active'
-       AND (campus = $1 OR $1 IS NULL)`,
-      [campus]
+       AND campus = $1
+       LIMIT $2`,
+      [campus, limit]
     );
     return result.rows;
   }
@@ -40,43 +37,70 @@ export async function findProducts({ limit, campus }) {
 
 // Find products by category
 export async function findProductsByCategory({ category, limit, campus }) {
-  const result = await pool.query(
-    `SELECT * 
-     FROM products 
-     WHERE category = $1 
-     AND state->>'state' = 'active' 
-     AND (campus = $2 OR $2 IS NULL)
-     LIMIT $3`,
-    [category, campus, limit]
-  );
-  return result.rows;
+  if (campus === null) {
+    const result = await pool.query(
+      `SELECT * 
+       FROM products 
+       WHERE category = $1 
+       AND state->>'state' = 'active' 
+       LIMIT $2`,
+      [category, limit]
+    );
+    return result.rows;
+  } else {
+    const result = await pool.query(
+      `SELECT * 
+       FROM products 
+       WHERE category = $1 
+       AND state->>'state' = 'active' 
+       AND campus = $2 
+       LIMIT $3`,
+      [category, campus, limit]
+    );
+    return result.rows;
+  }
 };
 
 // Find products by category and gender
 export async function findProductsByCategoryAndGender({ category, cap_gender, limit, campus }) {
+  if (campus === null) {
+    const result = await pool.query(
+      `SELECT * 
+       FROM products 
+       WHERE category = $1 
+       AND state->>'state' = 'active' 
+       AND others->>'gender' = $2 
+       LIMIT $3`,
+      [category, cap_gender, limit]
+    );
+    return result.rows;
+  } else {
+    const result = await pool.query(
+      `SELECT * 
+       FROM products 
+       WHERE category = $1 
+       AND state->>'state' = 'active' 
+       AND others->>'gender' = $2 
+       AND campus = $3 
+       LIMIT $4`,
+      [category, cap_gender, campus, limit]
+    );
+    return result.rows;
+  }
+};
+
+export async function findProductsType({ type, limit }) {
   const result = await pool.query(
-    `SELECT * 
-     FROM products 
-     WHERE category = $1 
-     AND state->>'state' = 'active' 
-     AND others->>'gender' = $2 
-     AND (campus = $3 OR $3 IS NULL)
-     LIMIT $4`,
-    [category, cap_gender, campus, limit]
+    `SELECT * FROM products 
+     WHERE state->>'state' = 'active' 
+     AND others->>'cType' = $1 
+     LIMIT $2`,
+    [type, limit]
   );
   return result.rows;
 };
 
-
-export async function findProductsType({ type, limit }) {
-   const result = await pool.query(
-      `SELECT * FROM products WHERE state->>'state' = 'active' AND others->>'cType' = $1 LIMIT $2`,
-      [type,limit]
-    );
-    return result.rows;
-};
-
-// Find product thumnail by ID
+// Find product thumbnail by ID
 export async function findProductsThumbnailById({ product_id }) {
   const result = await pool.query(
     `SELECT thumbnail_id FROM products WHERE product_id = $1`,
@@ -84,7 +108,6 @@ export async function findProductsThumbnailById({ product_id }) {
   );
   return result.rows;
 };
-
 
 // Create product view
 export async function createProductView({ product_id, user_id }) {
@@ -95,13 +118,12 @@ export async function createProductView({ product_id, user_id }) {
     ) VALUES (
         DEFAULT, $1, $2, $3, $4
     )`,
-    [shortId.generate(10), product_id, user_id, date]
-    );
+    [shortId.generate(), product_id, user_id, date]
+  );
     
   let response = await errorHandler(result?.rowCount);
   return response;
 };
-
 
 // Find product view by ID
 export async function findProductViewById({ product_id, user_id }) {
@@ -112,8 +134,7 @@ export async function findProductViewById({ product_id, user_id }) {
   return result.rows;
 };
 
-
-// UPdate product view by ID
+// Update product view by ID
 export async function updateProductView({ product_id }) {
   const result = await pool.query(
     `UPDATE products set views = views+1 WHERE product_id = $1`,
