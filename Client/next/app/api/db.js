55b54cb07fda6c653d3e664eaeddc44bbc536aaa
Achifa-devs <akpulufabian@gmail.com>
2025-09-29 16:03:35 +0100

    db url updated

--- Client/next/app/api/db.js
+++ Client/next/app/api/db.js
@@ -1,7 +1,7 @@
 // lib/db.js
 import { Pool } from 'pg';
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://achifa.io.llc:cflV8XEbCO7h@ep-billowing-sunset-28191429-pooler.us-east-2.aws.neon.tech/neondb";
+const DATABASE_URL = "postgres://achifa.io.llc:cflV8XEbCO7h@ep-billowing-sunset-28191429-pooler.us-east-2.aws.neon.tech/neondb";
 
 // Force IPv4 by adding `?options=--inet-family=ipv4`
 const connectionString = DATABASE_URL.includes('?')
