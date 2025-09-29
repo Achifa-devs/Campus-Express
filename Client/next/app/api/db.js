9abba0dbd8cc4b834d7a4ce07a6271278aef9ca2
Achifa-devs <akpulufabian@gmail.com>
2025-09-29 18:47:15 +0100

    killer

--- Client/next/app/api/db.js
+++ Client/next/app/api/db.js
@@ -1,15 +1,13 @@
 // lib/db.js
 import { Pool } from 'pg';
 
-const DATABASE_URL = "postgresql://achifa.io.llc:cflV8XEbCO7h@ep-small-block-a55csuu3-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+const DATABASE_URL = "postgresql://achifa.io.llc:cflV8XEbCO7h@ep-billowing-sunset-28191429-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
 
 // Force IPv4 by adding `?options=--inet-family=ipv4`
-const connectionString = DATABASE_URL.includes('?')
-  ? `${DATABASE_URL}`
-  : `${DATABASE_URL}`;
+const connectionString = DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString,
   ssl: {
     rejectUnauthorized: false, // required by Neon
   },
